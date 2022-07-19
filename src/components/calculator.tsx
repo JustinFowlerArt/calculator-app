400e889c3715e4eae56ea05d1852d4a3b9a3576b
Justin Fowler <jusfowle@gmail.com>
2022-07-19 13:35:07 -0400

    Add logic to improve calculations and handle edge cases

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -2,31 +2,44 @@ import { useState } from 'react';
 import { Input } from './input';
 
 export const Calculator = () => {
-    const [operand, setOperand] = useState('')
+	const [operator, setOperator] = useState('');
+	const [firstOperand, setFirstOperand] = useState(0);
+	const [secondOperand, setSecondOperand] = useState(0);
 	const [currentValue, setCurrentValue] = useState('0');
 
 	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-		const { value } = e.currentTarget;
+		let { value } = e.currentTarget;
 		switch (value) {
-			case '7':
-			case '8':
-			case '9':
-			case '4':
-			case '5':
-			case '6':
-			case '1':
-			case '2':
-			case '3':
-			case '0': {
-				if (currentValue === '0') {
-					setCurrentValue(value);
+			case '=': {
+				const newValue = evaluate();
+				setCurrentValue(newValue);
+				break;
+			}
+			case '+':
+			case '-':
+			case 'x':
+			case '/': {
+				if (value === 'x') {
+					value = '*';
+				}
+				if (currentValue.search(/[+\-*/]+/) > 0) {
+					const newValue = evaluate();
+					setCurrentValue(newValue + value);
 				} else {
 					setCurrentValue(currentValue + value);
 				}
+				setOperator(value);
 				break;
 			}
 			case '.': {
-				if (currentValue.indexOf('.') > -1) {
+				const values = currentValue.split(/[+\-*/]+/);
+				if (values.length > 1) {
+					if (values[1].indexOf('.') > -1) {
+						break;
+					} else {
+						setCurrentValue(currentValue + value);
+					}
+				} else if (values[0].indexOf('.') > -1) {
 					break;
 				} else {
 					setCurrentValue(currentValue + value);
@@ -34,56 +47,77 @@ export const Calculator = () => {
 				break;
 			}
 			case 'DEL': {
-				if (currentValue.length <= 1) {
-					setCurrentValue('0');
+				if (currentValue.length <= 1 || currentValue === `Can't divide by 0`) {
+					reset();
 				} else {
+					const values = currentValue.split(/[+\-*/]+/);
+					if (values.length > 1) {
+						setSecondOperand(
+							parseFloat(values[1].slice(0, values[1].length - 1))
+						);
+					} else {
+						setFirstOperand(
+							parseFloat(values[0].slice(0, values[0].length - 1))
+						);
+					}
 					setCurrentValue(currentValue.slice(0, currentValue.length - 1));
 				}
 				break;
 			}
 			case 'RESET': {
-				setCurrentValue('0');
-				break;
-			}
-			case '=': {
-                const values = currentValue.split(/[+\-*/]+/)
-                let newValue = 0;
-                if (operand === '+') {
-                    newValue = parseFloat(values[0]) + parseFloat(values[1])
-                } else  if (operand === '-') {
-                    newValue = parseFloat(values[0]) - parseFloat(values[1])
-                } else  if (operand === '*') {
-                    newValue = parseFloat(values[0]) * parseFloat(values[1])
-                } else  if (operand === '/') {
-                    newValue = parseFloat(values[0]) / parseFloat(values[1])
-                } 
-                setCurrentValue(newValue.toString())
-				break;
-			}
-			case '+': {
-                setOperand('+')
-                setCurrentValue(currentValue + value);
-				break;
-			}
-            case '-': {
-                setOperand('-')
-                setCurrentValue(currentValue + value);
-				break;
-			}
-            case 'x': {
-                setOperand('*')
-                setCurrentValue(currentValue + '*');
-				break;
-			}
-            case '/': {
-                setOperand('/')
-                setCurrentValue(currentValue + value);
+				reset();
 				break;
 			}
 			default: {
+				let values = [];
+				if (currentValue === '0' || currentValue === `Can't divide by 0`) {
+					setCurrentValue(value);
+					values.push(value);
+				} else if (currentValue.startsWith('-')) {
+                    setCurrentValue(currentValue + value);
+                    values = (currentValue + value).split(/[+\-*/]+/);
+                    values = values.slice(1, values.length)
+                    values[0] = '-' + values[0]
+				} else {
+					setCurrentValue(currentValue + value);
+					values = (currentValue + value).split(/[+\-*/]+/);
+				}
+				setFirstOperand(parseFloat(values[0]));
+				if (values.length > 1) {
+					setSecondOperand(parseFloat(values[1]));
+				}
 				break;
 			}
 		}
+	};
+
+	const reset = () => {
+		setOperator('');
+		setFirstOperand(0);
+		setSecondOperand(0);
+		setCurrentValue('0');
+	};
+
+	const evaluate = () => {
+		let newValue: number | string = 0;
+		if (operator === '+') {
+			newValue = firstOperand + secondOperand;
+		} else if (operator === '-') {
+			newValue = firstOperand - secondOperand;
+		} else if (operator === '*') {
+			newValue = firstOperand * secondOperand;
+		} else if (operator === '/') {
+			if (secondOperand === 0) {
+				newValue = `Can't divide by 0`;
+				reset();
+			} else {
+				newValue = firstOperand / secondOperand;
+			}
+		}
+		if (newValue !== `Can't divide by 0`) {
+			setFirstOperand(parseFloat(newValue.toString()));
+		}
+		return newValue.toString();
 	};
 
 	const inputs = [
@@ -110,7 +144,11 @@ export const Calculator = () => {
 	return (
 		<main className='flex flex-col space-y-6'>
 			<div className='flex bg-dark-background-screen p-6 rounded-xl justify-end'>
-				<span className='text-4xl font-bold'>{currentValue}</span>
+				<span className='text-4xl font-bold'>
+					{currentValue === '0'
+						? parseFloat(currentValue).toLocaleString('en-US')
+						: currentValue}
+				</span>
 			</div>
 			<div className='grid grid-cols-4 grid-rows-5 gap-4 bg-dark-background-keyboard p-6 rounded-xl'>
 				{inputs.map(input => (

d731edbfde23d1ed28da749b6571d33731fe6194
Justin Fowler <jusfowle@gmail.com>
2022-07-20 17:36:14 -0400

    Simplify theme toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,42 +11,36 @@ export const Header = ({ changeTheme, theme }: Props) => {
 				<h2 className='text-xs font-semibold tracking-wider mr-6'>
 					THEME
 				</h2>
-				<form className='relative flex justify-between h-6 w-16 bg-secondary py-1 px-3 rounded-2xl'>
+				<form className='relative flex justify-between h-6 w-16 bg-secondary py-1 px-3 rounded-2xl text-center'>
 					<label className='flex flex-col items-center'>
-						<span className='absolute top-[-1.25rem] text-xs'>1</span>
+						<span className='absolute top-[-1.25rem] text-xs w-4 h-10'>1</span>
 						<input
 							type='radio'
 							name='theme'
-                            id='1'
 							value='dark'
                             className='hidden'
 							onClick={e => changeTheme(e.currentTarget.value)}
 						></input>
-                        <label htmlFor='1' className='w-3.5 h-3.5 absolute top-[.3rem] rounded-full '></label>
 					</label>
 					<label className='flex flex-col items-center'>
-						<span className='absolute top-[-1.25rem] text-xs'>2</span>
+						<span className='absolute top-[-1.25rem] text-xs w-4 h-10'>2</span>
 						<input
 							type='radio'
 							name='theme'
-                            id='2'
 							value='light'
                             className='hidden'
 							onClick={e => changeTheme(e.currentTarget.value)}
 						></input>
-                        <label htmlFor='2' className='w-3.5 h-3.5 absolute top-[.3rem] left-6.5 rounded-full '></label>
 					</label>
 					<label className='flex flex-col items-center'>
-						<span className='absolute top-[-1.25rem] text-xs'>3</span>
+						<span className='absolute top-[-1.25rem] text-xs w-4 h-10'>3</span>
 						<input
 							type='radio'
 							name='theme'
-                            id='3'
 							value='contrast'
                             className='hidden'
 							onClick={e => changeTheme(e.currentTarget.value)}
 						></input>
-                        <label htmlFor='3' className='w-3.5 h-3.5 absolute top-[.3rem] rounded-full '></label>
 					</label>
                     <div className={`w-3.5 h-3.5 absolute top-[.3rem] bg-tertiary-key rounded-full ${theme === 'light' ? 'left-6' : theme === 'contrast' ? 'right-1.5' : 'left-1.5'}`}></div>
 				</form>

95a94badc535907bea013ec9e447da09c54f426e
Justin Fowler <jusfowle@gmail.com>
2022-07-20 18:34:21 -0400

    Set theme from system preference and store changes in local storage

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Calculator } from './components/calculator';
 import { Header } from './components/header';
 
 export const App = () => {
-	const [theme, setTheme] = useState('dark');
+	const [theme, setTheme] = useState('');
+
+	useEffect(() => {
+		const getCurrentTheme = () =>
+			window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (localStorage.theme) {
+            setTheme(localStorage.theme)
+        } else if (getCurrentTheme()) {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	}, []);
 
 	const changeTheme = (theme: string) => {
 		setTheme(theme);
+		localStorage.theme = theme;
 	};
 
 	return (
-		<div className={`flex justify-center h-screen p-8 bg-primary ${theme === 'light' ? 'light text-primary' : theme === 'contrast' ? 'contrast text-primary' : 'text-white'}`}>
+		<div
+			className={`flex justify-center h-screen p-8 bg-primary ${
+				theme === 'light'
+					? 'light text-primary'
+					: theme === 'contrast'
+					? 'contrast text-primary'
+					: 'text-white'
+			}`}
+		>
 			<div className='flex flex-col space-y-8 w-full max-w-lg'>
-				<Header changeTheme={changeTheme} theme={theme}/>
+				<Header changeTheme={changeTheme} theme={theme} />
 				<Calculator />
 			</div>
 		</div>

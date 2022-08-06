5a74ed7946a8298b79c6307109bb09ce09a12597
Justin Fowler <jusfowle@gmail.com>
2022-08-06 13:38:04 -0400

    Update font, README, minor style tweaks, add screenshots

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,14 +6,14 @@ interface Props {
 export const Input = ({ value, handleClick }: Props) => {
 	return (
 		<button
-			className={`text-3xl font-bold rounded-lg border-b-4 hover:brightness-150 ${
+			className={`text-3xl font-bold rounded-lg border-b-4 hover:brightness-150 lg:py-3 ${
 				value === '='
 					? 'col-span-2 text-xl text-tertiary bg-tertiary-key border-tertiary-shadow'
 					: value === 'RESET'
 					? 'col-span-2 text-xl text-secondary bg-secondary-key border-secondary-shadow'
 					: value === 'DEL'
-					? 'aspect-square text-xl text-secondary bg-secondary-key border-secondary-shadow'
-					: 'aspect-square text-primary bg-primary-key border-primary-shadow'
+					? 'aspect-square text-xl text-secondary bg-secondary-key border-secondary-shadow lg:aspect-auto'
+					: 'aspect-square text-primary bg-primary-key border-primary-shadow lg:aspect-auto'
 			} `}
 			value={value}
 			onClick={e => handleClick(e)}
